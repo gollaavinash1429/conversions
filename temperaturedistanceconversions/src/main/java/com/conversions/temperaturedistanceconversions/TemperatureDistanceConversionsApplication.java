af00d83f35c57b6f8bf099edd8d6e404ec725281
gollaavinash1429 <64130012+gollaavinash1429@users.noreply.github.com>
2020-04-22 10:19:28 +0200

    Add files via upload

---
+++ temperaturedistanceconversions/src/main/java/com/conversions/temperaturedistanceconversions/TemperatureDistanceConversionsApplication.java
@@ -0,0 +1,13 @@
+package com.conversions.temperaturedistanceconversions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureDistanceConversionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureDistanceConversionsApplication.class, args);
+	}
+
+}
---
+++ temperaturedistanceconversions/src/test/java/com/conversions/temperaturedistanceconversions/TemperatureDistanceConversionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversions.temperaturedistanceconversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureDistanceConversionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
